bd241d8d525a600407d3a3877332311a01b726ad
heckwithu2 <jheck10@kent.edu>
2022-04-12 21:18:58 -0400

    first commit

---
+++ portfolioBackEnd/Controllers/InformationController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace portfolioBackEnd.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class informationController : ControllerBase
+    {
+
+        [HttpGet]
+        public String Get()
+        {
+            //Get Information from table
+            return "Hello World";
+        }
+    }
+}
---
+++ portfolioBackEnd/Tables/Information.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace portfolioBackEnd
+{
+    public class Information
+    {
+        public int idInformation { get; set; }
+
+        public String firstName { get; set; }
+
+        public String LastName { get; set; }
+
+        public String github { get; set; }
+
+        public String linkedin { get; set; }
+
+        public String introduction { get; set; }
+    }
+}
